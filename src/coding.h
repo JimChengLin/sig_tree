49fbe498d0c09e7bec96fc7e524a585ee37e3b5b
JimChengLin <jimzuolin@gmail.com>
2018-12-23 16:39:57 +0800

    remove UB

--- src/coding.h
+++ src/coding.h
@@ -3,16 +3,21 @@
 #define SIG_TREE_CODING_H
 
 #include <cstdint>
+#include <cstring>
 
 namespace sgt {
     static_assert(sizeof(uint8_t) == sizeof(char));
 
     inline char Uint8ToChar(uint8_t i) {
-        return *reinterpret_cast<char *>(&i);
+        char c;
+        memcpy(&c, &i, sizeof(c));
+        return c;
     }
 
     inline uint8_t CharToUint8(char c) {
-        return *reinterpret_cast<uint8_t *>(&c);
+        uint8_t i;
+        memcpy(&i, &c, sizeof(i));
+        return i;
     }
 }
 
