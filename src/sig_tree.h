82bbf5f91b2250d96b8f094d5cdbaa4cb5fb3f84
JimChengLin <jimzuolin@gmail.com>
2019-10-02 01:38:02 +0800

    faster mget

--- src/sig_tree.h
+++ src/sig_tree.h
@@ -327,7 +327,7 @@ namespace sgt {
             kForward = false,
             kBackward = true,
             kMajorVersion = 1,
-            kMinorVersion = 8,
+            kMinorVersion = 9,
             kMaxKeyLength = std::numeric_limits<K_DIFF>::max() >> 3
         };
 
--- src/sig_tree_mop_impl.h
+++ src/sig_tree_mop_impl.h
@@ -15,86 +15,61 @@ namespace sgt {
     auto SignatureTreeTpl<KV_TRANS, K_DIFF, KV_REP>::
     MultiGetWithCallback(const Slice * ks,
                          CALLBACK && callback) {
-        struct GetContext {
-            enum State {
-                kFindBestMatchImpl = 0,
-                kIsPacked,
-                kEnd
-            };
-
-            int state;
-            Node * cursor;
-            KV_REP * rep;
-            Slice k;
-
-            inline static void Init(GetContext * ctx, SignatureTreeTpl * tree, const Slice & k) {
-                ctx->state = kFindBestMatchImpl;
-                ctx->cursor = tree->OffsetToMemNode(tree->kRootOffset);
-                ctx->rep = nullptr;
-                ctx->k = k;
-
-                if (SGT_UNLIKELY(NodeSize(ctx->cursor) == 0)) {
-                    ctx->state = kEnd;
-                }
+        std::array<KV_REP *, N> reps{};
+        Node * root = OffsetToMemNode(kRootOffset);
+        if (SGT_UNLIKELY(NodeSize(root) == 0)) {
+            if constexpr (std::is_same<CALLBACK, std::false_type>::value) {
+                return reps;
+            } else {
+                return callback(reps);
             }
-
-            inline static void Next(GetContext * ctx, SignatureTreeTpl * tree) {
-                switch (ctx->state) {
-                    case kFindBestMatchImpl: {
-                        size_t idx;
-                        bool direct;
-                        std::tie(idx, direct, std::ignore) = FindBestMatchImpl(ctx->cursor, ctx->k);
-                        ctx->rep = &ctx->cursor->reps_[idx + direct];
-#ifndef SGT_NO_MM_PREFETCH
-                        _mm_prefetch(ctx->rep, _MM_HINT_T0);
-#endif
-                        break;
-                    }
-
-                    case kIsPacked: {
-                        auto & r = *ctx->rep;
-                        if (tree->IsPacked(r)) {
-                            ctx->cursor = tree->OffsetToMemNode(tree->Unpack(r));
-#ifndef SGT_NO_MM_PREFETCH
-                            _mm_prefetch(&ctx->cursor->size_, _MM_HINT_T0);
-                            auto p = reinterpret_cast<const char *>(&ctx->cursor->diffs_);
-                            p -= reinterpret_cast<uintptr_t>(p) % 64;
-                            _mm_prefetch(p + 64 * 0, _MM_HINT_T2);
-                            _mm_prefetch(p + 64 * 1, _MM_HINT_T2);
-                            _mm_prefetch(p + 64 * 2, _MM_HINT_T2);
-                            _mm_prefetch(p + 64 * 3, _MM_HINT_T2);
-                            _mm_prefetch(p + 64 * 4, _MM_HINT_T2);
-#endif
-                            ctx->state = kFindBestMatchImpl;
-                            return;
-                        }
-                        break;
-                    }
-
-                    default:
-                        assert(ctx->state == kEnd);
-                        return;
-                }
-                ++ctx->state;
-            }
-        };
-
-        std::array<GetContext, N> ctxs;
-        for (size_t i = 0; i < N; ++i) {
-            GetContext::Init(&ctxs[i], this, ks[i]);
         }
+
+        std::array<Node *, N> cursors;
+        cursors.fill(root);
 
         size_t remaining;
         do {
             remaining = N;
-            for (auto & ctx :ctxs) {
-                GetContext::Next(&ctx, this);
-                remaining -= (ctx.state == GetContext::kEnd);
+
+            for (size_t i = 0; i < N; ++i) {
+                Node * cursor = cursors[i];
+                if (cursor != nullptr) {
+                    size_t idx;
+                    bool direct;
+                    std::tie(idx, direct, std::ignore) = FindBestMatchImpl(cursor, ks[i]);
+#ifndef SGT_NO_MM_PREFETCH
+                    _mm_prefetch(reps[i] = &cursor->reps_[idx + direct], _MM_HINT_T0);
+#else
+                    reps[i] = &cursor->reps_[idx + direct];
+#endif
+                }
+            }
+
+            for (size_t i = 0; i < N; ++i) {
+                Node *& cursor = cursors[i];
+                if (cursor != nullptr) {
+                    const auto & rep = *reps[i];
+                    if (IsPacked(rep)) {
+                        cursor = OffsetToMemNode(Unpack(rep));
+#ifndef SGT_NO_MM_PREFETCH
+                        _mm_prefetch(&cursor->size_, _MM_HINT_T0);
+                        auto p = reinterpret_cast<const char *>(&cursor->diffs_);
+                        p -= reinterpret_cast<uintptr_t>(p) % 64;
+                        _mm_prefetch(p + 64 * 0, _MM_HINT_T2);
+                        _mm_prefetch(p + 64 * 1, _MM_HINT_T2);
+                        _mm_prefetch(p + 64 * 2, _MM_HINT_T2);
+                        _mm_prefetch(p + 64 * 3, _MM_HINT_T2);
+                        _mm_prefetch(p + 64 * 4, _MM_HINT_T2);
+#endif
+                        continue;
+                    }
+                    cursor = nullptr;
+                }
+                --remaining;
             }
         } while (remaining != 0);
 
-        std::array<KV_REP *, N> reps;
-        for (size_t i = 0; i < N; ++i) { reps[i] = ctxs[i].rep; }
         if constexpr (std::is_same<CALLBACK, std::false_type>::value) {
             return reps;
         } else {
